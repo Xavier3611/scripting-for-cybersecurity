3035dee54cf2d0a0a3020b9d35c82451109051be
Xavier3611 <c00319941@setu.ie>
2026-09-14 11:02:15 +0000

    Complete LAB 1

---
+++ lab01/hello.py
@@ -0,0 +1,9 @@
+
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+
+course = input("What course are you studying? ")
+print("You are studying  + course)
+
+area = input("What area of cybersecurity interests you?")
+print( "You are interested in  + area)
---
+++ lab01/student.py
@@ -0,0 +1,8 @@
+
+name = input("What is your name? ")
+studentNumber = input("What is your student number? ")
+year = input("What year you in?")
+
+print("Name: " + name)
+print("Student number: " + studentNumber)
+print("Year: "+ year)
